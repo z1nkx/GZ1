f2f39c6a4453d38ab1a44f762c66419dfdf6713c
z1nkx <117737787+z1nkx@users.noreply.github.com>
2023-04-09 08:57:07 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,10 +105,10 @@ const Home: NextPage = () => {
         <div
           className={styles.optionSelectBox}
           role="button"
-          onClick={() => router.push("")}
+          onClick={() => router.push("https://ipfs.thirdwebcdn.com/ipfs/QmbAgC8YwY36n8H2kuvSWsRisxDZ15QZw3xGZyk9aDvcv7/marketplace.html?contract=0x3a44bbF6Eb3f6C65a8dA074822256a26Dda18080&chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%7D&listingId=5&theme=dark")}
         >
           {/* Burn Tokens */}
-          <h2 className={styles.selectBoxTitle}>MARKETPLACE </h2>
+          <h2 className={styles.selectBoxTitle}>AUCTION </h2>
           <p className={styles.selectBoxDescription}>
             COMING SOON
           </p>
